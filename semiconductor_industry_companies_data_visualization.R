f00d29a9b6e774cba1f2bdb359534536bab4fd84
Lee-Way Wang <lee.way56@comcast.net>
2021-09-01 20:49:36 -0700

    Initial commit
    - Create gitignore
    - First commit for data visualization script
    - Initial commit for RStudio project file

---
+++ semiconductor_industry_companies_data_visualization.R
@@ -0,0 +1,11 @@
+# Visualizes several statistics of TSMC, Intel, Samsung Electronics, and SMIC.
+
+library(here)
+library(rio)
+library(dplyr)
+library(ggplot2)
+
+
+read_stock_data <- function(){
+    
+}
