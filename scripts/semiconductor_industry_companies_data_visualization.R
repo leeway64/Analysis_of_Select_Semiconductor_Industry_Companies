39d7cd8fd13e807497c6e1ef821ebc90d4253b0a
Lee-Way Wang <lee.way56@comcast.net>
2021-09-04 18:40:34 -0700

    plot_revenue, update gitignore
    - Plot revenue for Intel, TSMC, Samsung, and SMIC
    - Update gitignore to ignore certain Microsoft Office files

--- scripts/semiconductor_industry_companies_data_visualization.R
+++ scripts/semiconductor_industry_companies_data_visualization.R
@@ -7,7 +7,7 @@ library(ggplot2)
 
 
 read_data <- function(data_file){
-    data <- import(here("data", data_file))
+    data <- import_list(here("data", data_file))
     return(data)
 }
 
@@ -16,6 +16,21 @@ plot_stock_price <- function(){
     
 }
 
+
 plot_revenue <- function(){
+    revenue_data = read_data("revenue/revenue.xlsx")
+    Intel <- revenue_data$Intel
+    TSMC <- revenue_data$TSMC
+    Samsung <- revenue_data$Samsung
+    SMIC <- revenue_data$SMIC
     
+    dev.new()
+    print(ggplot()+
+        geom_line(aes(x=Intel$Year, y=Intel$Revenue), color='blue')+
+            geom_line(aes(x=TSMC$Year, y=TSMC$Revenue), color='red')+
+            geom_line(aes(x=Samsung$Year, y=Samsung$Revenue), color='grey')+
+            geom_line(aes(x=SMIC$Year, y=SMIC$Revenue), color='yellow')+
+            labs(title='Revenue vs. date', x='Date', y='Revenue (USD)'))
 }
+
+plot_revenue()
